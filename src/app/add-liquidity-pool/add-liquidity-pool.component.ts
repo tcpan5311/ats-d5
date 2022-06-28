543b9f79a1d122dda4255988aef2193eba33ec9c
Tcpan0412 <tzechernp0412@gmail.com>
2022-06-29 05:22:23 +0800

    Add liquidity component addition

--- src/app/add-liquidity-pool/add-liquidity-pool.component.ts
+++ src/app/add-liquidity-pool/add-liquidity-pool.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
+import {DialogService} from 'primeng/dynamicdialog';
+import {Message,MessageService} from 'primeng/api';
 
 @Component({
   selector: 'app-add-liquidity-pool',
   templateUrl: './add-liquidity-pool.component.html',
-  styleUrls: ['./add-liquidity-pool.component.scss']
+  styleUrls: ['./add-liquidity-pool.component.scss'],
+  providers: [DialogService,MessageService]
 })
 export class AddLiquidityPoolComponent implements OnInit {
 
